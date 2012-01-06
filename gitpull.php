65a2b909158c152f403e245ab3481c7fe29d4cd9
Rex Lorenzo <rex@oid.ucla.edu>
2012-01-06 13:14:43 -0800

    server doesn't have php-posix, so just use whoami to get current user

--- gitpull.php
+++ gitpull.php
@@ -8,7 +8,7 @@
 require 'config.php';
 
 // make sure that script is run from command line and as specified repo user
-if (get_username_from_uid(posix_geteuid()) == $repo_user && is_cli()) {
+if (get_script_user() == $repo_user && is_cli()) {
     // now run git pull for given repo
     $output = array();
     $return_var = null;
@@ -26,38 +26,12 @@ echo 'Script needs to be run on command line as specified user';
 exit(1);
 
 /**
- * Sometimes posix_getpwuid is not always installed, so try other methods of
- * getting username from uid.
- * 
- * http://www.php.net/manual/en/function.posix-getpwuid.php#104737
- * 
- * @param int $uid
- * @return string       Returns username for given uid. If could not find 
- *                      username, then just returns uid back again
+ * For some reason php-posix is not installed by default in our server, so just 
+ * run command line command 'whoami' and get result
  */
-function get_username_from_uid($uid) {
-    if (function_exists('posix_getpwuid')) {
-        $a = posix_getpwuid($uid);
-        return $a['name'];
-    } elseif (strstr(php_uname('s'), 'BSD')) {
-        # This works on BSD but not with GNU 
-        exec('id -u ' . (int) $uid, $o, $r);
-
-        if ($r == 0) {
-            return trim($o['0']);
-        }else {
-            return $uid;
-        }
-    } elseif (is_readable('/etc/passwd')) {
-        exec(sprintf('grep :%s: /etc/passwd | cut -d: -f1', (int) $uid), $o, $r);
-        if ($r == 0) {
-            return trim($o['0']);
-        } else {
-            return $uid;
-        }
-    } else {
-        return $uid;
-    }
+function get_script_user()
+{
+    return exec('whoami');
 }
 
 /**
