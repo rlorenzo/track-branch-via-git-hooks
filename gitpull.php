30147a011ad8de23840e7913db112aa408569091
Rex Lorenzo <rexlorenzo@gmail.com>
2016-11-16 10:21:16 -0800

    Adding support for submodules in submodules

--- gitpull.php
+++ gitpull.php
@@ -18,7 +18,7 @@ if (get_script_user() == $repo_user && is_cli()) {
     $return_var = null;
     
     // update repo and any submodules
-    $cmd = sprintf("cd %s && /usr/bin/git pull && /usr/bin/git submodule sync && /usr/bin/git submodule update --init", $repo_location);
+    $cmd = sprintf("cd %s && /usr/bin/git pull && /usr/bin/git submodule sync && /usr/bin/git submodule update --init --recursive", $repo_location);
     
     debug('executing command: ' . $cmd);
     exec($cmd, $output, $return_var);
